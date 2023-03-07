ff944b0a8ff1fb812105d41c8aebc6c69545f120
thuyvt <thuyvt1005@gmail.com>
2023-03-07 15:54:43 +0700

    day1

---
+++ src/Day1/Main.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Day1;
+
+/**
+ *
+ * @author ThuyVT
+ */
+public class Main {
+//    public static void main(String args[]) {
+//        
+//    }
+
+    public static void main(String[] args) {
+        // C1: Khong xuong dong
+        System.out.print("Hello world");
+        System.out.print("Java 1\n");
+        // C2: Co duong dong
+        System.out.println("Toi ten la Vu Thi Thuy");
+        System.out.println("IT18310");
+        System.out.println("Nam nay toi 18 tui");
+        int tuoi = 32;
+        String soDienThoai = "0886341201";
+        double luong = 600.1234;
+        // C3: Hien thi voi dinh dang du lieu
+        System.out.printf("So dien thoai %s, luong la %.2f, nam nay %d tuoi\n", soDienThoai, luong, tuoi);
+        System.out.println("So dien thoai " + soDienThoai + ", luong la " + luong +", nam nay " + tuoi + " tuoi");
+    }
+}
